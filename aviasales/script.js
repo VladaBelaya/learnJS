7558f243d8b0ad3a1282ef22ad06314066a4348f
VladaBelaya <vlada_chvanova@mail.ru>
2021-11-24 17:55:08 +0300

    by clicking a city from the list appears in the input

--- aviasales/script.js
+++ aviasales/script.js
@@ -5,13 +5,35 @@ const       formSearch = document.querySelector('.form-search'),
       dropdownCitiesTo = formSearch.querySelector('.dropdown__cities-to'),
        inputDateDepart = formSearch.querySelector('.input__date-depart');
 const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Керч', 'Волгоград', 'Самара', 'Днепропетровск', 'Екатеринбург', 'Одесса', 'Ухань', 'Шымкен', 'Нижний Новгород', 'Калининград', 'Вроцлав', 'Ростов-на-Дону']
+const showCities = (input, list) => {
+   list.textContent = ''
+   if (input.value.trim() !== '') {
+      const filterCity = city.filter(item => {
+         const fixItem = item.toLowerCase()
+         return fixItem.includes(input.value.toLowerCase())
+      })
+
+      filterCity.forEach(item => {
+         const li = document.createElement('li')
+         li.classList.add('dropdown__city')
+         li.textContent = item
+         list.append(li)
+      })
+   }
+
+   
+   
+}
 
 inputCitiesFrom.addEventListener('input', () => {
-   const filterCity = city.filter(item => {
-      const fixItem = item.toLowerCase()
-      return fixItem.includes(inputCitiesFrom.value)
-   })
-   console.log(filterCity)
+   showCities(inputCitiesFrom, dropdownCitiesFrom)
+})
+
+dropdownCitiesFrom.addEventListener('click', (event) => {
+   if(event.target.tagName.toLowerCase() === 'li') {
+      inputCitiesFrom.value = event.target.textContent
+      dropdownCitiesFrom.textContent = ''
+   }
 })
 
     
